baf7c925a186727c7b16e057cf937f238a15aea6
autumn-sun91 <dpwe231@gmail.com>
2025-07-22 01:48:06 +0900

    feat. 일일 이체 한도 구현 - 한도 내에서 이체 가능

--- src/main/kotlin/com/sendy/infrastructure/processor/TransferProcessor.kt
+++ src/main/kotlin/com/sendy/infrastructure/processor/TransferProcessor.kt
@@ -4,8 +4,11 @@ import com.sendy.domain.enum.TransactionHistoryTypeEnum
 import com.sendy.domain.transfer.TransactionHistory
 import com.sendy.domain.transfer.TransferLimit
 import com.sendy.domain.transfer.TransferLimitCountProcessor
+import com.sendy.infrastructure.persistence.transfer.TransferLimitJpaEntity
 import com.sendy.infrastructure.persistence.transfer.TransferLimitJpaRepository
+import com.sendy.support.util.getTsid
 import org.springframework.stereotype.Component
+import java.time.LocalDateTime
 
 @Component
 class TransferProcessor(
@@ -17,25 +20,55 @@ class TransferProcessor(
         dailyDt: String,
         callback: (withdrawTxList: List<TransactionHistory>, transferLimit: TransferLimit) -> Unit,
     ): TransferLimit {
-        val transferLimitJpaEntity = transferLimitJpaRepository.findByUserIdAndDailyDt(userId, dailyDt)
-        val withdrawTxList = transactions.filter { it.type == TransactionHistoryTypeEnum.WITHDRAW }
+        val findEntity = transferLimitJpaRepository.findByUserIdAndDailyDt(userId, dailyDt)
+
         val transferLimit =
-            TransferLimit(
-                id = transferLimitJpaEntity!!.id,
-                dailyDt = transferLimitJpaEntity.dailyDt,
-                dailyLimit = transferLimitJpaEntity.dailyLimit,
-                singleTransactionLimit = transferLimitJpaEntity.singleTransactionLimit,
-                dailyCount = transferLimitJpaEntity.dailyCount,
-                createdAt = transferLimitJpaEntity.createdAt,
-                updatedAt = transferLimitJpaEntity.updatedAt,
-                userId = transferLimitJpaEntity.userId,
-            )
+            if (findEntity == null) {
+                TransferLimit(
+                    id = getTsid(),
+                    dailyDt = dailyDt,
+                    dailyLimit = 10_000_000,
+                    singleTransactionLimit = 1_000_000,
+                    dailyCount = 0,
+                    createdAt = LocalDateTime.now(),
+                    updatedAt = LocalDateTime.now(),
+                    userId = userId,
+                )
+            } else {
+                TransferLimit(
+                    id = findEntity.id,
+                    dailyDt = findEntity.dailyDt,
+                    dailyLimit = findEntity.dailyLimit,
+                    singleTransactionLimit = findEntity.singleTransactionLimit,
+                    dailyCount = findEntity.dailyCount,
+                    createdAt = findEntity.createdAt,
+                    updatedAt = findEntity.updatedAt,
+                    userId = findEntity.userId,
+                )
+            }
+
+        val withdrawTxList = transactions.filter { it.type == TransactionHistoryTypeEnum.WITHDRAW }
 
         callback.invoke(withdrawTxList, transferLimit)
 
-        transferLimitJpaEntity.dailyCount = transferLimit.dailyCount
+        if (findEntity == null) {
+            transferLimitJpaRepository.save(
+                TransferLimitJpaEntity(
+                    id = transferLimit.id,
+                    dailyDt = transferLimit.dailyDt,
+                    dailyLimit = transferLimit.dailyLimit,
+                    singleTransactionLimit = transferLimit.singleTransactionLimit,
+                    dailyCount = transferLimit.dailyCount,
+                    createdAt = transferLimit.createdAt,
+                    updatedAt = transferLimit.updatedAt,
+                    userId = transferLimit.userId,
+                ),
+            )
+        } else {
+            findEntity.dailyCount = transferLimit.dailyCount
 
-        transferLimitJpaRepository.save(transferLimitJpaEntity)
+            transferLimitJpaRepository.save(findEntity)
+        }
 
         return transferLimit
     }

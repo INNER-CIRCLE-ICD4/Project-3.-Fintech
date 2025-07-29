11b66d0de5450c7e576d3a584ca0c0c3005190e6
autumn-sun91 <dpwe231@gmail.com>
2025-07-29 22:04:40 +0900

    feat: 바로 송금하기 (1)

--- src/main/kotlin/com/sendy/domain/account/AccountRepository.kt
+++ src/main/kotlin/com/sendy/domain/account/AccountRepository.kt
@@ -1,7 +1,10 @@
 package com.sendy.domain.account
 
 import com.sendy.domain.account.AccountEntity
+import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
 
 interface AccountRepository : JpaRepository<AccountEntity, Long> {
     fun findByUserId(userId: Long): List<AccountEntity>
@@ -10,4 +13,8 @@ interface AccountRepository : JpaRepository<AccountEntity, Long> {
         userId: Long,
         accountNumber: String,
     ): AccountEntity?
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select account from AccountEntity account where 1=1 and account.accountNumber = :accountNumber")
+    fun findOneByAccountNumberForUpdate(accountNumber: String): AccountEntity
 }
--- src/main/kotlin/com/sendy/infrastructure/config/JpaConfig.kt
+++ src/main/kotlin/com/sendy/infrastructure/config/JpaConfig.kt
@@ -1,8 +1,14 @@
 package com.sendy.infrastructure.config
 
+import jakarta.persistence.EntityManagerFactory
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.orm.jpa.JpaTransactionManager
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
+import org.springframework.transaction.PlatformTransactionManager
+import javax.sql.DataSource
 
 /**
  *
@@ -11,4 +17,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 @Configuration
 @EnableJpaAuditing
 @EnableJpaRepositories(basePackages = ["com.sendy.domain", "com.sendy.infrastructure.persistence"])
-class JpaConfig
+class JpaConfig {
+    @Bean
+    fun entityManagerFactory(datasource: DataSource): LocalContainerEntityManagerFactoryBean {
+        val emf = LocalContainerEntityManagerFactoryBean()
+        emf.dataSource = datasource
+
+        return emf
+    }
+
+    @Bean
+    fun platformTransactionManager(emf: EntityManagerFactory): PlatformTransactionManager = JpaTransactionManager(emf)
+}

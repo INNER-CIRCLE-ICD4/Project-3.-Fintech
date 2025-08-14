0602f09e2ab8b8b2104b672494576790fa071c1d
autumn-sun91 <dpwe231@gmail.com>
2025-08-14 19:07:11 +0900

    feat. 비동기 실험

--- sendy-transfer-scheduler/build.gradle.kts
+++ sendy-transfer-scheduler/build.gradle.kts
@@ -4,4 +4,7 @@ version = "0.0.1-SNAPSHOT"
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     api(project(":sendy-transfer-domain"))
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.2")
 }

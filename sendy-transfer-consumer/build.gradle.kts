79650b7c234978e654f9435c307aabb949f7162f
autumn-sun91 <dpwe231@gmail.com>
2025-09-04 20:55:39 +0900

    feat: 애플리케이션용 Dockerfile 추가

--- sendy-transfer-consumer/build.gradle.kts
+++ sendy-transfer-consumer/build.gradle.kts
@@ -1,6 +1,11 @@
 group = "com.sendy.transfer-consumer"
 version = "0.0.1-SNAPSHOT"
 
+tasks.bootJar {
+    archiveFileName.set("sendy-trasnfer-consumer.jar")
+    mainClass.set("com.sendy.transferConsumer.SendyTransferConsumerApplicationKt")
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     api(project(":sendy-transfer-domain"))
--- sendy-transfer-scheduler/build.gradle.kts
+++ sendy-transfer-scheduler/build.gradle.kts
@@ -1,6 +1,11 @@
 group = "com.sendy.transfer-scheduler"
 version = "0.0.1-SNAPSHOT"
 
+tasks.bootJar {
+    archiveFileName.set("sendy-trasnfer-scheduler.jar")
+    mainClass.set("com.sendy.transferScheduler.SendyTransferSchedulerApplicationKt")
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     api(project(":sendy-transfer-domain"))
